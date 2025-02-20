273ce81b5106269b5c1dfa583b3b87fd55b8cd41
Makeisz <139496644+Makeisz@users.noreply.github.com>
2025-02-20 10:28:22 +0300

    Update mario1.c

--- mario1.c
+++ mario1.c
@@ -1,1 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height:");
+    }
+    while (height < 1);
+    int hash_holder = 1;
+    int hash = 1;
+    int space;
+
+    while (height > 0)
+    {
+        space = height - 1;
+        while (space > 0)
+        {
+            printf(" ");
+            space--;
+        }
+        if (space == 0)
+        {
+            hash_holder = hash;
+            while (hash_holder > 0)
+            {
+                printf("#");
+                hash_holder--;
+            }
+        }
+        printf("\n");
+        hash++;
+        height--;
+    }
+}
